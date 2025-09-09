ce1d628668ce5aa8afad7f6a514f3884c5468a8b
Abel Asmelash <abelasmelash07@gmail.com>
2025-09-09 09:36:09 +0000

    module css added

--- amazon-clone/src/components/Header/Header.jsx
+++ amazon-clone/src/components/Header/Header.jsx
@@ -1,61 +1,61 @@
 import React from "react";
-import "./Header.css";
+import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className={styles.header}>
       {/* Left Section */}
-      <div className="header__left">
+      <div className={styles.headerLeft}>
         <a href="/">
           <img
-            className="header__logo"
+            className={styles.headerLogo}
             src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
             alt="amazon_logo"
           />
         </a>
-        <div className="header__location">
-          <p className="header__optionLineOne">Delivered to</p>
-          <span className="header__optionLineTwo">Netherlands</span>
+        <div className={styles.headerLocation}>
+          <p className={styles.optionLineOne}>Delivered to</p>
+          <span className={styles.optionLineTwo}>Netherlands</span>
         </div>
       </div>
 
       {/* Middle Section (Search bar) */}
-      <div className="header__search">
-        <select className="header__searchDropdown">
+      <div className={styles.headerSearch}>
+        <select className={styles.headerSearchDropdown}>
           <option value="all">All</option>
         </select>
         <input
-          className="header__searchInput"
+          className={styles.headerSearchInput}
           type="text"
           placeholder="Search product"
         />
-        <button className="header__searchIcon">🔍</button>
+        <button className={styles.headerSearchIcon}>🔍</button>
       </div>
 
       {/* Right Section */}
-      <div className="header__right">
-        <div className="header__language">
+      <div className={styles.headerRight}>
+        <div className={styles.headerLanguage}>
           <img
             src="https://www.shutterstock.com/shutterstock/photos/1704274045/display_1500/stock-vector-netherlands-circle-button-flag-national-symbol-icon-vector-illustrarion-1704274045.jpg"
             alt="flag"
           />
           <select>
-            <option value="en">NL</option>
+            <option value="en">EN</option>
           </select>
         </div>
 
-        <div className="header__option">
-          <span className="header__optionLineOne">Hello, Sign in</span>
-          <span className="header__optionLineTwo">Account & Lists</span>
+        <div className={styles.headerOption}>
+          <span className={styles.optionLineOne}>Hello, Sign in</span>
+          <span className={styles.optionLineTwo}>Account & Lists</span>
         </div>
 
-        <div className="header__option">
-          <span className="header__optionLineOne">Returns</span>
-          <span className="header__optionLineTwo">& Orders</span>
+        <div className={styles.headerOption}>
+          <span className={styles.optionLineOne}>Returns</span>
+          <span className={styles.optionLineTwo}>& Orders</span>
         </div>
 
-        <div className="header__cart">
-          🛒 <span className="header__optionLineTwo">Cart</span>
+        <div className={styles.headerCart}>
+          🛒 <span className={styles.optionLineTwo}>Cart</span>
         </div>
       </div>
     </header>
