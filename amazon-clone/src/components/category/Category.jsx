15896c3b313d0f93ee8a8796596a98eef0410283
Abel Asmelash <abelasmelash07@gmail.com>
2025-09-10 15:54:33 +0000

    category section finilized

--- amazon-clone/src/components/category/Category.jsx
+++ amazon-clone/src/components/category/Category.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { categoryData } from './categoryInfo';
 import CategoryCard from './CategoryCard';
+import styles from './Category.module.css'; // Import CSS module
 
 const Category = () => {
   return (
-    <div className="category-container">
-      <section className="category-section">
+    <div className={styles.categoryContainer}>
+      <section className={styles.categorySection}>
         {categoryData.map((infos, index) => (
           <CategoryCard key={index} data={infos} />
         ))}
